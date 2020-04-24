e28c37b8db78a6c8388b82259bec7b19b5d226ab
Elias <elias.007@live.se>
2020-04-24 13:45:13 +0200

    Python scriptgit add .git add .

--- AutoGetConfig.py
+++ AutoGetConfig.py
@@ -1,6 +1,6 @@
 from subprocess import call
 
 call('sudo mv /srv/tftp/sw1_running_config /home/pi/switchconfigs/', shell=True)
-call('sudo mv /srv/tftp/hp_switch_configuration /home/pi/switchconfigs/', shell=True)
+call('sudo mv /srv/tftp/hp_switch_configuration /home/pi/switchconfigs/hpConfigs/', shell=True)
 
 
